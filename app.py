4389a0e6ec22bef239a0c98e6ae1aa83167a5930
hashtag-squirrel <alita2000@web.de>
2023-11-14 13:11:48 +0100

    Add mildew detector dashboard page and supporting scripts

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from app_pages.multipage import MultiPage
 # load pages scripts
 from app_pages.page_summary import page_summary_body
 from app_pages.page_leaf_visualizer import page_leaf_visualizer_body
-# from app_pages.page_powdery_mildew_detector import page_powdery_mildew_detector_body  # noqa
+from app_pages.page_powdery_mildew_detector import page_powdery_mildew_detector_body  # noqa
 # from app_pages.page_project_hypotheses import page_project_hypotheses_body
 # from app_pages.page_ml_performance import page_ml_performance_metrics
 
@@ -15,7 +15,7 @@ app = MultiPage(app_name="Powdery Mildew Detector")
 # Add your app pages here using .add_page()
 app.add_page("Quick Project Summary", page_summary_body)
 app.add_page("Leaf Visualiser", page_leaf_visualizer_body)
-# app.add_page("Powdery Mildew Detection", page_powdery_mildew_detector_body)
+app.add_page("Powdery Mildew Detection", page_powdery_mildew_detector_body)
 # app.add_page("Project Hypotheses", page_project_hypotheses_body)
 # app.add_page("ML Performance Metrics", page_ml_performance_metrics)
 

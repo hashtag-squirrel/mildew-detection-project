2dcb23718cd140b8f22e704ec3ad1ea43f16907e
hashtag-squirrel <alita2000@web.de>
2023-11-14 12:07:30 +0100

    Add project summary page for Streamlit dashboard

---
+++ app.py
@@ -0,0 +1,22 @@
+# Code adapted from Code Institute's Malaria walkthrough project
+
+from app_pages.multipage import MultiPage
+
+# load pages scripts
+from app_pages.page_summary import page_summary_body
+# from app_pages.page_leaf_visualizer import page_leaf_visualizer_body
+# from app_pages.page_powdery_mildew_detector import page_powdery_mildew_detector_body  # noqa
+# from app_pages.page_project_hypotheses import page_project_hypotheses_body
+# from app_pages.page_ml_performance import page_ml_performance_metrics
+
+# Create an instance of the app
+app = MultiPage(app_name="Powdery Mildew Detector")
+
+# Add your app pages here using .add_page()
+app.add_page("Quick Project Summary", page_summary_body)
+# app.add_page("Leaf Visualiser", page_leaf_visualizer_body)
+# app.add_page("Powdery Mildew Detection", page_powdery_mildew_detector_body)
+# app.add_page("Project Hypotheses", page_project_hypotheses_body)
+# app.add_page("ML Performance Metrics", page_ml_performance_metrics)
+
+app.run()  # Run the app
---
+++ app_pages/page_summary.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+
+
+def page_summary_body():
+
+    st.write("### Quick Project Summary")
+
+    st.info(
+        "**General Information**\n"
+        "* Powdery mildew is a fungal disease that affects a wide range of "
+        "plants. Powdery mildew diseases are caused by many different species "
+        "of ascomycete fungi in the order Erysiphales.Powdery mildew is one of "
+        "the easier plant diseases to identify, as its symptoms are quite "
+        "distinctive.\n"
+        "* The fictitious company Farmy & Foods has large cherry plantations "
+        "and have lately encountered powdery mildew on their cherry trees. "
+        "Currently, the process to identify the affected trees is manual and "
+        "quite time-consuming. The company wants to make use of an ML model "
+        "to predict whether or not a tree is affected on images of leaves "
+        "their staff supplies.\n"
+
+        "**Project Dataset**\n"
+        "* The available dataset contains 4208 images of cherry leaves "
+        "supplied by Farmy & Foods, taken from their crops.\n"
+        )
+
+    st.write(
+        "* For additional information, please visit and **read** the "
+        "[Project README file](https://github.com/hashtag-squirrel/mildew-detection-project/blob/main/README.md).")
+
+    st.success(
+        "The project has 2 business requirements:\n"
+        "* 1 - The client is interested in conducting a study to visually "
+        "differentiate a healthy cherry leaf from one with powdery mildew.\n"
+        "* 2 - The client is interested in predicting if a cherry leaf is "
+        "healthy or contains powdery mildew.\n"
+        )
